f0e5749e17a82de2e2d64029e8d2f1f28b72dbee
drewbuckaroo <drewmlockhart@gmail.com>
2020-08-14 14:53:14 -0700

    add hide to all remaining else if statements.

--- js/scripts.js
+++ js/scripts.js
@@ -10,56 +10,108 @@ $(document).ready(function() {
         $(".python").hide();
       } else if (guitar === "sg" && transp === "van" && music === "rock") {
         $(".ruby").show();
+        $(".c").hide();
+        $(".python").hide();
       } else if (guitar === "sg" && transp === "blades" && music === "rock") {
         $(".ruby").show();
+        $(".c").hide();
+        $(".python").hide();
       } else if (guitar === "sg" && transp === "moto" && music === "cumbia") {
         $(".c").show();
+        $(".ruby").hide();
+        $(".python").hide();
       } else if (guitar === "sg" && transp === "van" && music === "cumbia") {
         $(".c").show();
+        $(".ruby").hide();
+        $(".python").hide();
       } else if (guitar === "sg" && transp === "blades" && music === "cumbia") {
         $(".c").show();
+        $(".ruby").hide();
+        $(".python").hide();
       } else if (guitar === "sg" && transp === "moto" && music === "techno") {
         $(".python").show();
+        $(".ruby").hide();
+        $(".c").hide();
       }  else if (guitar === "sg" && transp === "van" && music === "techno") {
         $(".python").show();
+        $(".ruby").hide();
+        $(".c").hide();
       }  else if (guitar === "sg" && transp === "blades" && music === "techno") {
         $(".python").show();
+        $(".ruby").hide();
+        $(".c").hide();
       } else if (guitar === "tele" && transp === "moto" && music === "rock") {
         $(".ruby").show();
+        $(".python").hide();
+        $(".c").hide();
       } else if (guitar === "tele" && transp === "van" && music === "rock") {
-        $(".ruby").show();   
+        $(".ruby").show();
+        $(".python").hide();
+        $(".c").hide();   
       } else if (guitar === "tele" && transp === "blades" && music === "rock") {
         $(".ruby").show();
+        $(".python").hide();
+        $(".c").hide();
       } else if (guitar === "tele" && transp === "moto" && music === "cumbia") {
         $(".c").show();
+        $(".python").hide();
+        $(".ruby").hide();
       } else if (guitar === "tele" && transp === "van" && music === "cumbia") {
         $(".c").show();
+        $(".python").hide();
+        $(".ruby").hide();
       } else if (guitar === "tele" && transp === "blades" && music === "cumbia") {
         $(".c").show();
+        $(".python").hide();
+        $(".ruby").hide();
       } else if (guitar === "tele" && transp === "moto" && music === "techno") {
         $(".python").show();
+        $(".c").hide();
+        $(".ruby").hide();
       }  else if (guitar === "tele" && transp === "van" && music === "techno") {
         $(".python").show();
+        $(".c").hide();
+        $(".ruby").hide();
       }  else if (guitar === "tele" && transp === "blades" && music === "techno") {
         $(".python").show();
+        $(".c").hide();
+        $(".ruby").hide();
       } else if (guitar === "v" && transp === "moto" && music === "rock") {
         $(".ruby").show();
+        $(".c").hide();
+        $(".python").hide();
       } else if (guitar === "v" && transp === "van" && music === "rock") {
-        $(".ruby").show();   
+        $(".ruby").show();
+        $(".c").hide();
+        $(".python").hide();   
       } else if (guitar === "v" && transp === "blades" && music === "rock") {
         $(".ruby").show();
+        $(".c").hide();
+        $(".python").hide();
       } else if (guitar === "v" && transp === "moto" && music === "cumbia") {
         $(".c").show();
+        $(".ruby").hide();
+        $(".python").hide();
       } else if (guitar === "v" && transp === "van" && music === "cumbia") {
         $(".c").show();
+        $(".ruby").hide();
+        $(".python").hide();
       } else if (guitar === "v" && transp === "blades" && music === "cumbia") {
         $(".c").show();
+        $(".ruby").hide();
+        $(".python").hide();
       } else if (guitar === "v" && transp === "moto" && music === "techno") {
         $(".python").show();
+        $(".ruby").hide();
+        $(".c").hide();
       }  else if (guitar === "v" && transp === "van" && music === "techno") {
         $(".python").show();
+        $(".ruby").hide();
+        $(".c").hide();
       }  else if (guitar === "v" && transp === "blades" && music === "techno") {
         $(".python").show();
+        $(".ruby").hide();
+        $(".c").hide();
       }
 
 
